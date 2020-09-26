82932d34110d7856142704974076924387e6eeb5
plmerideth <plmerideth@gmail.com>
2020-09-25 21:23:40 -0600

    fixing buttons

--- js/main.js
+++ js/main.js
@@ -12,18 +12,18 @@ function displayPortList()
 	[
 		{label: "URL: Week 1 Notes",	url: "week1/index1.html"},
 		{label: "URL: Week 2 Notes",	url: "week2/index2.html"},
-		{label: "URL: Week 3 TBD",	url: "week2/index3.html"},
-		{label: "URL: Week 4 TBD",	url: "week2/index4.html"},
-		{label: "URL: Week 5 TBD",	url: "week2/index5.html"},
-		{label: "URL: Week 6 TBD",	url: "week2/index6.html"},
-		{label: "URL: Week 7 TBD",	url: "week2/index7.html"},
-		{label: "URL: Week 8 TBD",	url: "week2/index8.html"},
-		{label: "URL: Week 9 TBD",	url: "week2/index9.html"},
-		{label: "URL: Week 10 TBD",	url: "week2/index10.html"},
-		{label: "URL: Week 11 TBD",	url: "week2/index11.html"},
-		{label: "URL: Week 12 TBD",	url: "week2/index12.html"},
-		{label: "URL: Week 13 TBD",	url: "week2/index13.html"},
-		{label: "URL: Week 14 TBD",	url: "week2/index14.html"}
+		{label: "URL: Week 3 TBD",	url: "week3/index3.html"},
+		{label: "URL: Week 4 TBD",	url: "week4/index4.html"},
+		{label: "URL: Week 5 TBD",	url: "week5/index5.html"},
+		{label: "URL: Week 6 TBD",	url: "week6/index6.html"},
+		{label: "URL: Week 7 TBD",	url: "week7/index7.html"},
+		{label: "URL: Week 8 TBD",	url: "week8/index8.html"},
+		{label: "URL: Week 9 TBD",	url: "week9/index9.html"},
+		{label: "URL: Week 10 TBD",	url: "week10/index10.html"},
+		{label: "URL: Week 11 TBD",	url: "week11/index11.html"},
+		{label: "URL: Week 12 TBD",	url: "week12/index12.html"},
+		{label: "URL: Week 13 TBD",	url: "week13/index13.html"},
+		{label: "URL: Week 14 TBD",	url: "week14/index14.html"}
 		//Add new links and labels for each week her
 	]	
 	
