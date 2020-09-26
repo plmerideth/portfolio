91e7a43f533e7fec170afda165e301a337ae65d9
plmerideth <plmerideth@gmail.com>
2020-09-25 20:01:51 -0600

    Updated commit for week 2

--- js/main.js
+++ js/main.js
@@ -5,23 +5,25 @@ displayPortList();
 function displayPortList()
 {
 	//t x = document.getElementById("portfolioIndex");
+	//Set week = to current week to be displayed.  This selects class to be applied to links
+	let week = 2;
 	
 	const links =
 	[
 		{label: "URL: Week 1 Notes",	url: "week1/index1.html"},
 		{label: "URL: Week 2 Notes",	url: "week2/index2.html"},
-		{label: "URL: Week 3 Notes",	url: "week2/index3.html"},
-		{label: "URL: Week 4 Notes",	url: "week2/index4.html"},
-		{label: "URL: Week 5 Notes",	url: "week2/index5.html"},
-		{label: "URL: Week 6 Notes",	url: "week2/index6.html"},
-		{label: "URL: Week 7 Notes",	url: "week2/index7.html"},
-		{label: "URL: Week 8 Notes",	url: "week2/index8.html"},
-		{label: "URL: Week 9 Notes",	url: "week2/index9.html"},
-		{label: "URL: Week 10 Notes",	url: "week2/index10.html"},
-		{label: "URL: Week 11Notes",	url: "week2/index11.html"},
-		{label: "URL: Week 12 Notes",	url: "week2/index12.html"},
-		{label: "URL: Week 13 Notes",	url: "week2/index13.html"},
-		{label: "URL: Week 14 Notes",	url: "week2/index14.html"}
+		{label: "URL: Week 3 TBD",	url: "week2/index3.html"},
+		{label: "URL: Week 4 TBD",	url: "week2/index4.html"},
+		{label: "URL: Week 5 TBD",	url: "week2/index5.html"},
+		{label: "URL: Week 6 TBD",	url: "week2/index6.html"},
+		{label: "URL: Week 7 TBD",	url: "week2/index7.html"},
+		{label: "URL: Week 8 TBD",	url: "week2/index8.html"},
+		{label: "URL: Week 9 TBD",	url: "week2/index9.html"},
+		{label: "URL: Week 10 TBD",	url: "week2/index10.html"},
+		{label: "URL: Week 11 TBD",	url: "week2/index11.html"},
+		{label: "URL: Week 12 TBD",	url: "week2/index12.html"},
+		{label: "URL: Week 13 TBD",	url: "week2/index13.html"},
+		{label: "URL: Week 14 TBD",	url: "week2/index14.html"}
 		//Add new links and labels for each week her
 	]	
 	
@@ -31,7 +33,15 @@ function displayPortList()
 		let li = document.createElement('li');
 		li.classList.add("liClass");
 		let a = document.createElement('a');
-		a.classList.add("aClass");
+		let weekNo = link.label.substring(10, 12);		
+		if(weekNo <= week)
+		{
+			a.classList.add("aClass");			
+		}
+		else
+		{
+			a.classList.add("aTBDClass");			
+		}
 		a.setAttribute('href', link.url);
 		a.innerText = link.label;
 		li.appendChild(a);
